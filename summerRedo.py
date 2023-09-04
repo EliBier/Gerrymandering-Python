7a4122bf7d574f47846c481ca657d08658717ed0
EliBier <110846506+EliBier@users.noreply.github.com>
2023-09-04 13:52:20 -0400

    Blocks and Counties Dataframes

---
+++ summerRedo.py
@@ -0,0 +1,36 @@
+#%%
+from random import random
+import shelve
+import threading
+from tkinter.tix import MAX
+import scipy.io
+import csv
+import pprint as pp
+from collections.abc import Callable, Iterable
+from pathlib import Path
+from queue import Queue
+import numpy as np
+import networkx as nx
+import pandas as pd
+import random
+import time
+
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+from matplotlib.lines import Line2D
+
+data_path = Path("data")
+N_DISTRICTS = 13
+MIN_DISTRICT_POP = 400000
+MAX_DISTRICT_POP = 1000000
+
+LOGISTIC_K = 0.75
+LOGISTIC_MIDPOINT = 700000
+LOGISTIC_SCALE_FACTOR = 100000
+LOGISTIC_MIDPOINT /= LOGISTIC_SCALE_FACTOR
+
+#%%
+Blocks = pd.read_csv(data_path / "Blocks.csv", index_col="BlockID")
+Counties = pd.read_csv(data_path / "Counties.csv", index_col="CountyID")
+
+# %%
